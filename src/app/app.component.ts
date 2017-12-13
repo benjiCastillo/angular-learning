68b8d350a30027053303858c6b5101000ee1ca7a
benji Castillo <castilloeguezbenji@gmail.com>
2017-12-13 03:48:42 -0400

    two way binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,10 +9,12 @@ export class AppComponent {
   title:string ;
   name:string;
   listo:boolean ;
+  name1:string;
 
   constructor(){
     this.title = "Angular";
     this.name = "Benji";
+    this.name1 = "";
     this.listo = false;
     setTimeout(() => {
       this.listo = true;
