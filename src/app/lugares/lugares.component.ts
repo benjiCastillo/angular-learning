3f4622ff00bcfefa48a7b4d038ee8f01959dd075
benjiCastillo <castilloeguezbenji@gmail.com>
2018-01-23 23:21:24 -0400

    animations terminado

--- src/app/lugares/lugares.component.ts
+++ src/app/lugares/lugares.component.ts
@@ -9,14 +9,10 @@ import {trigger,state,style, transition,animate} from "@angular/animations";
   animations:[
     trigger('contenedorAnimable',[
       state('inicial',style({
-        opacity:0,
-        backgroundColor: 'rebeccapurple',
-        transform: 'rotate3d(0,0,0,0deg)'
+        opacity:0
       })),
       state('final',style({
-        opacity:1,
-        backgroundColor:'blue',
-        transform:'rotate3d(5,10,20,30deg)'
+        opacity:1
       })),
       transition('inicial => final',animate(1000)),
       transition('final => inicial',animate(1000)),
@@ -29,13 +25,14 @@ export class LugaresComponent implements OnInit {
   title: string = 'Mi casa';
   zoom: number = 15;
   lugares = null;
-  state:string = 'final'; 
+  state:string = 'inicial'; 
 
   constructor(private lugaresServices: LugaresService) {
     lugaresServices.getLugares()
     .subscribe(
       lugares=>{
         this.lugares = lugares;
+        this.animar();
         console.log("bien")
       }, error =>{
         console.log(error)
